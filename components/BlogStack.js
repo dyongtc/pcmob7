8126dbbe9a5a25ca77299a222a09e79cefca19a4
dion <dyongtc@gmail.com>
2022-07-14 09:37:27 +0800

    renamed innerstackScreens

--- components/BlogStack.js
+++ components/BlogStack.js
@@ -20,10 +20,10 @@ export default function BlogStack() {
 
   return (
     <InnerStack.Navigator>
-      <InnerStack.Screen name="Index" component={IndexScreen} options={{ title: "Blog", ...headerOptions, headerLeft: null }} />
-      <InnerStack.Screen name="Add" component={CreateScreen} options={{ title: "Add Post", ...headerOptions }} />
+      <InnerStack.Screen name="Index" component={IndexScreen} options={{ title: "List", ...headerOptions, headerLeft: null }} />
+      <InnerStack.Screen name="Add" component={CreateScreen} options={{ title: "Add Spend", ...headerOptions }} />
       <InnerStack.Screen name="Details" component={ShowScreen} options={headerOptions} />
-      <InnerStack.Screen name="Edit" component={EditScreen} options={{ title: "Edit Post", ...headerOptions }} />
+      <InnerStack.Screen name="Edit" component={EditScreen} options={{ title: "Edit Spend", ...headerOptions }} />
     </InnerStack.Navigator>
   )
 }
